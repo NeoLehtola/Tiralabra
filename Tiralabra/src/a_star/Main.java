3364a1eb7633208993bd48bf27db7b17dd18e503
Neo Lehtola <pklehtol@cs.helsinki.fi>
2012-07-31 19:57:19 +0300

    a* pakkaus ja main

---
+++ Tiralabra/src/a_star/Main.java
@@ -0,0 +1,14 @@
+package a_star;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author neom
+ */
+public class Main {
+    
+}
