f2cf9fa8a662d42636bf6d59f64882350c330b0e
Neo Lehtola <pklehtol@cs.helsinki.fi>
2012-07-31 22:33:04 +0300

    nodeen getterit ja setterit

--- Tiralabra/src/a_star/Node.java
+++ Tiralabra/src/a_star/Node.java
@@ -3,7 +3,8 @@ package a_star;
 
 /**
  *
- * @author neom
+ * Tämä luokka kapseloi yhden solmun toteutuksen; se sisältää tämänhetkisen pelitilanteen
+ * sekä funktion f = g + h arvot
  */
 public class Node {
     
@@ -12,9 +13,47 @@ public class Node {
     private int g;
     private int h;
     
-    public Node(int[] currentState) {
+    // tarviiko noden tietää parent ja successor?
+    public Node(int[] currentState, int g, int h) {
         this.currentState = currentState;
+        this.g = g;
+        this.h = h;
+        this.f = g + h;
     }
+    
+    // currentState sisältää tiedon nykyisestä pelitilanteesta taulukkomuodossa
+    public int[] getCurrentState() {
+        return currentState;
+    }
+    
+    // f = g + h
+    public void setF(int f) {
+        this.f = f;
+    }
+    
+    //  'g' is the sum of all the costs it took to get here
+    public void setG(int g) {
+        this.g = g;
+    }
+    
+    //  'h' is our heuristic function, the estimate of what it will take to get to the goal.
+    public void setH(int h) {
+        this.h = h;
+    }
+
+    public int getF() {
+        return f;
+    }
+
+    public int getG() {
+        return g;
+    }
+
+    public int getH() {
+        return h;
+    }
+    
+    
     
     
 }
