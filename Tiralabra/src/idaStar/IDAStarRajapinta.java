aa3ede18eaf6ca6f3da362cec5594a8e34bb6504
Neo Lehtola <pklehtol@cs.helsinki.fi>
2012-08-12 15:27:20 +0300

    idastar-rajapinnan hahmottelua

--- Tiralabra/src/idaStar/IDAStarRajapinta.java
+++ Tiralabra/src/idaStar/IDAStarRajapinta.java
@@ -1,31 +1,51 @@
-
 package idaStar;
 
 import sovelluslogiikka.Pelitapahtuma;
 
 /**
- * Tästä tulee samantyyppinen kuin IDDFS-rajapinnasta, mutta nodeille tulee heuristiikat.
- * Työnjaot luokkien välillä, ja näiden kahden mahd. yhteinen yliluokka tms. vielä harkinnassa
- * 
+ * Tästä tulee samantyyppinen kuin IDDFS-rajapinnasta, mutta nodeille tulee
+ * heuristiikat. Työnjaot luokkien välillä, ja näiden kahden mahd. yhteinen
+ * yliluokka tms. vielä harkinnassa
+ *
  */
 public class IDAStarRajapinta extends IDDFSRajapinta {
-    
-    
+
+    /**
+     *
+     * @param peli
+     */
     public IDAStarRajapinta(Pelitapahtuma peli) {
-        super(peli);   
+        super(peli);
+        getGoalNode().setH(0);
+    }
+
+    private int[] goalNodenXarvot(int laudanLeveys) {
+        return null;
     }
     
+    private int[] goalNodenYarvot(int laudanLeveys) {
+        return null;
+    }
+
+    /**
+     *
+     */
+    private void startNodenArvoH() {
+        int[] tilanne = getStartNode().getTilanne();
+
+    }
+
+    /**
+     *
+     * @param n
+     */
     public void manhattanDistance(Node n) {
         int[] state = n.getTilanne();
         int laudanLeveys = getPeli().getPelilauta().getLeveys();
-        
-        
-        
+
+
+
     }
-    
- 
-    
-    
     //    /**
 //     * tekee priority queueen nykyistä nodea seuraavat siirrot Huom. tätä pitää muokata,
 //     * priority queue tulee käyttöön vasta sitten heuristiikan kanssa, joten tämä luo nyt vääriä nodeja.
@@ -43,5 +63,4 @@ public class IDAStarRajapinta extends IDDFSRajapinta {
 //
 //        return jono;
 //    }
-    
 }
