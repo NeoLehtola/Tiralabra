207dbaeee70e4da38dbb57774df40972afdea31a
Neo <pklehtol@dhp-75.cs.helsinki.fi>
2012-08-20 18:53:21 +0300

    linkitetty pino toteutettu muttei testattu

--- Tiralabra/src/tietorakenteet/LinkitettyPino.java
+++ Tiralabra/src/tietorakenteet/LinkitettyPino.java
@@ -1,81 +1,91 @@
-
 package tietorakenteet;
 
+import java.util.NoSuchElementException;
+
 /**
- * 
- * 
+ *
+ *
  */
 public class LinkitettyPino<Item> {
-    
+
+    // http : //algs4.cs.princeton.edu/13stacks/Stack.java.html
     private Node eka;
     private int top;
-    
+
     /**
-     * 
+     *
      */
-    
     private class Node {
+
         private Item item;
         private Node prev;
         private Node next;
     }
-    
+
     public LinkitettyPino() {
         this.eka = null;
         this.top = 0;
     }
-    
+
     public boolean isEmpty() {
         return eka == null;
     }
-    
+
     public int size() {
         return this.top;
     }
-    
- 
+
     /**
-     * 
-     * @param uusi 
+     *
+     * @param uusi
      */
     public void push(Item uusi) {
         // MUISTA että pinossa eka on aina se päällimmäinen!
         Node edellinenEka = eka;
         eka = new Node();
         eka.item = uusi;
-        eka.next = edellinenEka;
-        
-        
+        eka.prev = edellinenEka;
+        edellinenEka.next = eka;
+
         top++;
     }
 
     /**
-     * 
-     * @return 
+     *
+     * @return
      */
     public int getTop() {
         return top;
     }
-    
+
     /**
-     * 
-     * @return 
+     *
+     * @return pinon päällimmäinen, poistetaan
      */
     public Item pop() {
-//        Item palautettava = taulukko[top-1];
-//        taulukko[top-1] = null;
-//        top--;
-//        return palautettava;
+        if (isEmpty()) {
+            throw new NoSuchElementException("Pino on tyhjä!");
+        }
+        Item palautettava = eka.item;
+        eka = eka.prev;
+        eka.next = null;
+        top--;
         
+        return palautettava;
+
+    }
+
+    /**
+     * 
+     * @return pinon päällimmäinen, ei poisteta
+     */
+    public Item peek() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Pino on tyhjä!");
+        }
+        return eka.item;
     }
     
 
-        
-        
-    }
-    
-    
-    
-    
     
 }
