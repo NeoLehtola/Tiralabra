8a199a0f82be5258802126a3aaa1fd2ddd8864ec
Neo <pklehtol@z1-2.cs.helsinki.fi>
2012-08-30 09:48:51 +0300

    muuttelin vaan mainia

--- Tiralabra/src/idaStar/Main.java
+++ Tiralabra/src/idaStar/Main.java
@@ -23,41 +23,35 @@ public class Main {
 
 
 
-//        int aikaKeskiarvo = 0;
-//        int siirtoKeskiarvo = 0;
-//        
-//        for (int i = 0; i < 10; i++) {
-//            Haku haku = new Haku(new Pelitapahtuma(3, 3, 1000));
-//            long aloitusAika = System.nanoTime();
-//            haku.iterativeDeepeningSearch(true, false);
-//            long lopetusAika = System.nanoTime();
-//            double aika = (lopetusAika - aloitusAika) / 1000000;
-//            System.out.println(aika + " ms, siirtoja " + haku.getSiirtojenMaara());
-//            aikaKeskiarvo += aika;
-//            siirtoKeskiarvo += haku.getSiirtojenMaara();
-//        }
-//        
-//        aikaKeskiarvo = aikaKeskiarvo /10;
-//        siirtoKeskiarvo = siirtoKeskiarvo / 10;
-//        System.out.println("Keskiarvo: " + aikaKeskiarvo + "ms");
-//        System.out.println("Siirtokeskiarvo: " + siirtoKeskiarvo + " siirtoa");
-
-
-
-
-
-//        if (haku.isRatkaisuLoytynyt()) {
-//            System.out.println("Löytyi!");
-//            
-//        }
-
-
-        Haku haku = new Haku(new Pelitapahtuma(3, 3, 1000));
-        haku.iterativeDeepeningSearch(true, false);
-
-        while (!haku.getReittiPino().isEmpty()) {
-            tulostaTaulukko(haku.getReittiPino().pop());
+        int aikaKeskiarvo = 0;
+        int siirtoKeskiarvo = 0;
+        int maara = 50;
+        
+        for (int i = 0; i < maara; i++) {
+            Haku haku = new Haku(new Pelitapahtuma(3, 3, 1000));
+            long aloitusAika = System.nanoTime();
+            haku.iterativeDeepeningSearch(true, false);
+            long lopetusAika = System.nanoTime();
+            double aika = (lopetusAika - aloitusAika) / 1000000;
+            System.out.println(aika + " ms, siirtoja " + haku.getSiirtojenMaara());
+            aikaKeskiarvo += aika;
+            siirtoKeskiarvo += haku.getSiirtojenMaara();
         }
+        
+        aikaKeskiarvo = aikaKeskiarvo / maara;
+        siirtoKeskiarvo = siirtoKeskiarvo / maara;
+        System.out.println("Keskiarvo: " + aikaKeskiarvo + "ms");
+        System.out.println("Siirtokeskiarvo: " + siirtoKeskiarvo + " siirtoa");
+
+
+
+
+//        Haku haku = new Haku(new Pelitapahtuma(3, 3, 1000));
+//        haku.iterativeDeepeningSearch(true, false);
+//
+//        while (!haku.getReittiPino().isEmpty()) {
+//            tulostaTaulukko(haku.getReittiPino().pop());
+//        }
 
 
 
